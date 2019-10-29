c7da448ec1e6dba953a38a40f91f11469bb4c169
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-10-29 09:19:52 +0530

    uploading code

---
+++ src/renderer/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import axios from 'axios';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+
+import App from './App';
+import router from './router';
+import store from './store';
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
+Vue.use(Buefy);
+Vue.http = Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
+
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>',
+}).$mount('#app');
---
+++ src/renderer/views/machine-settings.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <div class="block">
+      <b-radio v-model="radio" name="name" native-value="Flint">Flint</b-radio>
+      <b-radio v-model="radio" name="name" native-value="Silver">Silver</b-radio>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
